903d1b28ff5e1def9d08313d08dcc0a525fd5441
Shreyash Gupta <shreygupta12028@gmail.com>
2025-08-30 11:12:57 +0530

    build

--- app/api/items/[categoryId]/route.ts
+++ app/api/items/[categoryId]/route.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from "next/server";
+
+import { NextRequest, NextResponse } from "next/server";
+import dbConnect from "@/db/config";
 import Item from "@/models/Item";
+
 
 type Params = Promise<{ categoryId: string }>;
 
@@ -26,6 +29,43 @@ export async function GET(
     console.error("Error fetching items:", error);
 
     // Send a failure response with status 500
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+
+export async function PUT(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  try {
+    await dbConnect();
+
+    const { id } = context.params;
+    const { status } = await req.json();
+
+    const updatedItem = await Item.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+
+    if (!updatedItem) {
+      return NextResponse.json(
+        { message: "Item not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Item updated", item: updatedItem },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating item:", error);
     return NextResponse.json(
       { message: "Internal server error" },
       { status: 500 }
